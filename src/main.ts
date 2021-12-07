058e492fbaf667b76246f4966c82259eff37cf9d
Kevin Ingersoll <kingersoll@gmail.com>
2021-12-07 12:42:43 -0800

    add right click menu, couple small improvements

--- src/main.ts
+++ src/main.ts
@@ -1,60 +1,58 @@
+import { app, Tray, Menu, shell } from "electron";
 import { menubar } from "menubar";
 
-const mb = menubar({
-  dir: process.cwd(),
-  icon: `${process.cwd()}/images/menubar-icon/icon.white.png`,
-  tooltip: "MO Labs",
-  index: "https://life.molabs.ai/",
-  browserWindow: {
-    width: 800,
-    height: 600,
-  },
+// TODO: get app name + icons working
+//       we hide the dock, but still useful to have in e.g. activity monitor
+
+// TODO: fix the menu bar icon doing nothing on repeated click
+//       https://github.com/maxogden/menubar/issues/311
+
+app.on("ready", () => {
+  const tray = new Tray(`${process.cwd()}/images/menubar-icon/icon.white.png`);
+  tray.setToolTip("MO Labs");
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "About MO",
+      click: () => {
+        shell.openExternal("https://molabs.ai/info");
+      },
+    },
+    {
+      label: "Shop",
+      click: () => {
+        shell.openExternal("https://molabs.ai/shop");
+      },
+    },
+    { type: "separator" },
+    { role: "quit", label: "Quit MO" },
+  ]);
+  tray.on("right-click", () => {
+    tray.popUpContextMenu(contextMenu);
+  });
+
+  const mb = menubar({
+    tray,
+    dir: process.cwd(),
+    index: "https://life.molabs.ai/",
+    browserWindow: {
+      width: 800,
+      height: 600,
+    },
+  });
+
+  mb.on("ready", () => {
+    console.log("menubar app is ready");
+    mb.showWindow();
+
+    // If there's a dock icon, make sure tabbing over to it shows the window
+    mb.app.on("did-become-active", () => {
+      mb.showWindow();
+    });
+  });
+
+  // menubar is supposed to hide the dock icon by default, but it's not working so we'll do it manually
+  // https://github.com/maxogden/menubar/issues/306
+  mb.on("after-create-window", () => {
+    mb.app.dock.hide();
+  });
 });
-
-mb.on("ready", () => {
-  console.log("menubar app is ready");
-  mb.showWindow();
-});
-
-// import { app, BrowserWindow } from "electron";
-// import * as path from "path";
-
-// function createWindow() {
-//   const mainWindow = new BrowserWindow({
-//     webPreferences: {
-//       preload: path.join(__dirname, "preload.js"),
-//     },
-//     backgroundColor: "#000",
-//   });
-//   mainWindow.maximize();
-//   mainWindow.setMenu(null);
-
-//   mainWindow.loadURL("https://life.molabs.ai/");
-// }
-
-// // This method will be called when Electron has finished
-// // initialization and is ready to create browser windows.
-// // Some APIs can only be used after this event occurs.
-// app.on("ready", () => {
-//   createWindow();
-
-//   app.on("activate", function () {
-//     // On macOS it's common to re-create a window in the app when the
-//     // dock icon is clicked and there are no other windows open.
-//     if (BrowserWindow.getAllWindows().length === 0) {
-//       createWindow();
-//     }
-//   });
-// });
-
-// // Quit when all windows are closed, except on macOS. There, it's common
-// // for applications and their menu bar to stay active until the user quits
-// // explicitly with Cmd + Q.
-// app.on("window-all-closed", () => {
-//   if (process.platform !== "darwin") {
-//     app.quit();
-//   }
-// });
-
-// // In this file you can include the rest of your app"s specific main process
-// // code. You can also put them in separate files and require them here.
