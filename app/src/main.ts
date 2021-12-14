04fbe033fbabfe5e25d54ee8cf9e4b8e75c6cf96
Kevin Ingersoll <kingersoll@gmail.com>
2021-12-13 22:05:02 -0800

    rework without menubar

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,65 +1,66 @@
-import { app, Tray, Menu, shell } from "electron";
-import { menubar } from "menubar";
+import { app, Tray, Menu, shell, BrowserWindow, screen } from "electron";
 import path from "path";
 import "./updater";
 
-// TODO: fix the menu bar icon doing nothing on repeated click
-//       https://github.com/maxogden/menubar/issues/311
+// If we don't define these here, they get garbage collected which results in the e.g. tray disappearing
+let window: BrowserWindow;
+let tray: Tray;
 
 // Not sure why `productName` from `electron-builder.js` isn't being used for the app name, so we'll set it manually here.
 app.setName("MO");
 
 app.on("ready", () => {
-  const tray = new Tray(
-    path.join(__dirname, "..", "resources", "tray-icon.png")
-  );
+  const { width, height } = screen.getPrimaryDisplay().size;
+  window = new BrowserWindow({
+    width: width * 0.6,
+    height: height * 0.6,
+    autoHideMenuBar: true,
+    titleBarStyle: "hidden",
+  });
+
+  window.loadURL("https://life.molabs.ai/");
+
+  // Make the window draggable via anywhere in the body
+  window.webContents.insertCSS("body { -webkit-app-region: drag; }");
+
+  tray = new Tray(path.join(__dirname, "..", "resources", "tray-icon.png"));
   tray.setToolTip("MO");
 
-  const contextMenu = Menu.buildFromTemplate([
-    {
-      label: "About MO",
-      click: () => {
-        shell.openExternal("https://molabs.ai/info");
-      },
-    },
-    {
-      label: "Shop",
-      click: () => {
-        shell.openExternal("https://molabs.ai/shop");
-      },
-    },
-    { type: "separator" },
-    { role: "quit", label: "Quit MO" },
-  ]);
+  tray.on("click", () => {
+    window.show();
+  });
 
   tray.on("right-click", () => {
-    tray.popUpContextMenu(contextMenu);
-  });
-
-  const mb = menubar({
-    tray,
-    index: "https://life.molabs.ai/",
-    activateWithApp: true,
-    preloadWindow: true,
-    browserWindow: {
-      width: 800,
-      height: 600,
-    },
-  });
-
-  mb.on("ready", async () => {
-    mb.showWindow();
-
-    // If there's a dock icon, make sure tabbing over to it shows the window
-    mb.app.on("did-become-active", () => {
-      if (mb.window?.isVisible()) return;
-      mb.showWindow();
-    });
-  });
-
-  // menubar is supposed to hide the dock icon by default, but it's not working so we'll do it manually
-  // https://github.com/maxogden/menubar/issues/306
-  mb.on("after-create-window", () => {
-    mb.app.dock.hide();
+    tray.popUpContextMenu(
+      Menu.buildFromTemplate([
+        {
+          label: "Show MO",
+          click: () => {
+            window.show();
+          },
+        },
+        { type: "separator" },
+        {
+          label: "About MO",
+          click: () => {
+            shell.openExternal("https://molabs.ai/info");
+          },
+        },
+        {
+          label: "Shop",
+          click: () => {
+            shell.openExternal("https://molabs.ai/shop");
+          },
+        },
+        {
+          label: "Discord",
+          click: () => {
+            shell.openExternal("https://molabs.ai/discord");
+          },
+        },
+        { type: "separator" },
+        { role: "quit", label: "Quit MO" },
+      ])
+    );
   });
 });
